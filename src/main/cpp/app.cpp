609dc4c16182af039816ab61664c775787675456
ikpil <ikpil@naver.com>
2020-02-22 10:13:06 +0900

    챕터 1.4 시작

--- src/main/cpp/app.cpp
+++ src/main/cpp/app.cpp
@@ -11,6 +11,7 @@
 #include "chapter_1_1.h"
 #include "chapter_1_2.h"
 #include "chapter_1_3.h"
+#include "chapter_1_4.h"
 
 using namespace coding_skill;
 
@@ -19,6 +20,7 @@ int main () {
     chapters.push_back(std::make_shared<Chapter1_1>());
     chapters.push_back(std::make_shared<Chapter1_2>());
     chapters.push_back(std::make_shared<Chapter1_3>());
+    chapters.push_back(std::make_shared<Chapter1_4>());
 
     for (auto chapter : chapters)
     {
---
+++ src/main/cpp/chapter_1_4.cpp
@@ -0,0 +1,16 @@
+#include "chapter_1_4.h"
+#include <iostream>
+
+namespace coding_skill 
+{
+Chapter1_4::~Chapter1_4()
+{
+
+}
+
+void Chapter1_4::learn()
+{
+    std::cout << "Chapter 1.4 pass" << std::endl;
+}
+
+}
